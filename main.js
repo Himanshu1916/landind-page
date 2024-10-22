bbd4e8a2bdd2ab58e9a1c8aec42d23f841d8a625
himanshu1916 <hr8127884821@gmail.com>
2024-10-22 23:05:12 +0530

    counter added

--- main.js
+++ main.js
@@ -57,3 +57,28 @@ const scrollRevealOption = {
     ...scrollRevealOption,
     interval: 400,
   });
+
+
+
+  const counters = document.querySelectorAll('.value');
+const speed = 1500;
+
+counters.forEach( counter => {
+   const animate = () => {
+      const value = +counter.getAttribute('akhi');
+      const data = +counter.innerText;
+     
+      const time = value / speed;
+     if(data < value) {
+          counter.innerText = Math.ceil(data + time);
+          setTimeout(animate, 1);
+        }else{
+          counter.innerText = value;
+        }
+     
+   }
+   
+   animate();
+});
+
+
